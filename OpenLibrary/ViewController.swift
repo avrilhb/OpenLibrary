fc8aa037a639056c8872ad7f995e89700d11a335
Avril  Hernández <avril@macintosh.local>
2016-05-19 23:04:22 -0500

    First Commit

--- OpenLibrary/ViewController.swift
+++ OpenLibrary/ViewController.swift
@@ -10,9 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var isbn: UITextField!
+    @IBOutlet weak var resultadoEtiqueta: UILabel!
+    @IBOutlet weak var resultadoTextView: UITextView!
+    @IBOutlet weak var btnClear: UIButton!
+    
+    func sincrono(isbn:String) {
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:\(isbn)"
+        let url = NSURL(string: urls)
+        let datos: NSData? = NSData(contentsOfURL: url!)
+        
+        /*if datos == nil{
+            resultadoTextView.hidden = true
+            let alert = UIAlertController(title: "Error", message: "No hay conexión a Internet", preferredStyle: .Alert)
+            let cancelAction = UIAlertAction(title: "Ok", style: .Cancel, handler: nil)
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }else if texto == "{}" {
+            resultadoTextView.hidden = true
+            let alert = UIAlertController(title: "Error", message: "Libro no encontrado.", preferredStyle: .Alert)
+            let cancelAction = UIAlertAction(title: "Ok", style: .Cancel, handler: nil)
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+        }else{
+            resultadoTextView.hidden = false
+            self.resultadoTextView.text = texto! as String
+        }*/
+        
+        
+        
+        if datos == nil {
+            resultadoTextView.hidden = true
+            let alert = UIAlertController(title: "Error", message: "No hay conexión a Internet", preferredStyle: .Alert)
+            let cancelAction = UIAlertAction(title: "Ok", style: .Cancel, handler: nil)
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }else{
+            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            if texto == "{}" {
+                resultadoTextView.hidden = true
+                let alert = UIAlertController(title: "Error", message: "Libro no encontrado.", preferredStyle: .Alert)
+                let cancelAction = UIAlertAction(title: "Ok", style: .Cancel, handler: nil)
+                alert.addAction(cancelAction)
+                self.presentViewController(alert, animated: true, completion: nil)
+            }else{
+                resultadoTextView.hidden = false
+                self.resultadoTextView.text = texto! as String
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        resultadoTextView.hidden = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +77,12 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func textFieldDoneEditing(sender: AnyObject) {
+        sender.resignFirstResponder() //Desaparecer el teclado
+        sincrono(isbn.text!)
+        
+    }
+    @IBAction func clear(sender: AnyObject) {
+    }
 }
 
